e7565072d1565e35a6447257bf9f2399d32b4ef0
M-Munim <muhammadmunimoff330@gmail.com>
2026-01-25 22:05:57 +0500

    js class 1

---
+++ JS/Class1/script.js
@@ -0,0 +1,8 @@
+// class 1
+
+console.log("js from the external file");
+
+document.write("js from the external file");
+
+
+alert("logged in ")
---
+++ JS/Class1/script2.js
@@ -0,0 +1,81 @@
+let name = "Munim is my name"
+let age = 12
+age = 13
+let phoneNumber = 3219304654
+let phoneNumber2 = 3219304654.4
+
+let userEmail = "munim94@gmail.com"
+let isEmploye = true
+// let salary = undefined
+
+let vehicle = null;
+console.log(typeof vehicle)
+
+
+const country = "Pakistan"
+
+
+
+// console.log(age)
+
+let herName = "saima"
+herName = "alexa"
+
+// console.log(herName)
+
+
+
+// ===========================
+
+
+let stdData = {
+    // key : value 
+    name: "munim",
+    age: 24,
+    email: "munim@gmail.com",
+    address: "abc colony ",
+    city: ["peshawar", "islamabad", "mzd"],
+    phoneNumber: 3254324
+}
+
+// console.log(stdData.name, stdData.city)
+
+
+
+let empNames = ["munim", "saima", "alexa"]
+// console.log(empNames[1])
+
+
+let firstName = "M"
+let lastName = "Munim"
+console.log(firstName + lastName) 
+console.log(typeof firstName) 
+
+
+
+
+let firstNum = "4";
+let secondNum = 6
+let result = firstNum + secondNum
+
+console.log(result)
+console.log(typeof result)
+
+
+
+
+
+
+let salaryInStr = "90000"  // string
+
+let salaryInNum = Number(salaryInStr)
+
+console.log(typeof salaryInNum)
+
+
+
+let abc = 90000  // number
+
+let xyz = String(abc)
+
+console.log(typeof xyz)
